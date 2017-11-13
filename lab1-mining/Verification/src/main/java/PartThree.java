f9f3da6b5a1f30b3bf59e01355bf0e72203788b1
Robert Russo <rrusso44@icloud.com>
2017-11-13 11:03:59 -0500

    Verification Script
    This code is used for part 3 of the lab. In this portion the student will attempt to crack a JWT, which is essentialyl an encrypted cookie

---
+++ lab1-mining/Verification/src/main/java/PartThree.java
@@ -0,0 +1,44 @@
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Optional;
+
+public class PartThree {
+
+    private static Optional<String> createJWT(String secret) {
+        Algorithm algorithmHS;
+        Optional<String> token = Optional.empty();
+        try {
+            algorithmHS = Algorithm.HMAC256(secret);
+            token = Optional.of(JWT.create().withIssuer("ITWS").sign(algorithmHS));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return token;
+    }
+
+    private static boolean verifyJWT(String token, String secret) {
+        Algorithm algorithmHS;
+        try {
+            algorithmHS = Algorithm.HMAC256(secret);
+            JWT.require(algorithmHS).withIssuer("ITWS").build().verify(token);
+            return true;
+        } catch (UnsupportedEncodingException | JWTVerificationException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        Optional<String> jwt  = createJWT("test");
+        if (jwt.isPresent()) {
+            if (verifyJWT(jwt.get(), "test")) {
+                System.out.println("Cracked!");
+            } else {
+                System.out.println("Try Again");
+            }
+        }
+    }
+}
