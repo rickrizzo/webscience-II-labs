b1671d33ddfe88c2fc7b74d69d76afa83045ea24
machay <machay@rpi.edu>
2017-12-18 13:53:22 -0500

    Updated main.js
    Changed JSON names to be consistent

--- lab3-blockchain/main.js
+++ lab3-blockchain/main.js
@@ -9,11 +9,15 @@ var p2p_port = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 class Block {
-    constructor(index, previousHash, timestamp, data, hash) {
+    constructor(index, previousHash, timestamp, message, data, sender, receiver, amount, hash) {
         this.index = index;
         this.previousHash = previousHash.toString();
         this.timestamp = timestamp;
+        this.message = message;
         this.data = data;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
         this.hash = hash.toString();
     }
 }
@@ -26,7 +30,7 @@ var MessageType = {
 };
 
 var getGenesisBlock = () => {
-    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+    return new Block(0, "0", 1465154705, "This is the message!", "This is the Genesis Block!", "661243602", "661243603", "10", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
 var blockchain = [getGenesisBlock()];
